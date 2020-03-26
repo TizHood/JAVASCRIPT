3e1d8ce63b50d2a99b892584317dcc7ed99b6706
TizHood <62028239+TizHood@users.noreply.github.com>
2020-03-26 22:41:42 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let name = "Gbemi";
+console.log("My name is: " + name);
+const courses = ["Frontend", "Backend", "Mobile", "Design"];
+console.log(courses)
+const result = [];
+let courseNumber = courses.length;
+for (let i=0; i<=200; i++) {
+    if(courseNumber%2==0){
+        if(i % 2 == 0){
+            result.push(i);
+        }    
+     }else {"courses.push(i);" 
+if(i % 2 != 0){
+    result.push(i);
+    }
+}
+}
+console.log(result);
